a6315b142f537f3547aa7fbc396c9c76c03bc8ec
mikeyyy12 <shubhamkarmyal95@gmail.com>
2025-10-08 15:34:15 +0530

    update test:Initalize poll,fetch poll address

--- voting/tests/voting.ts
+++ voting/tests/voting.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Voting } from "../target/types/voting";
+import { PublicKey } from "@solana/web3.js";
 
 describe("voting", () => {
   // Configure the client to use the local cluster.
@@ -12,12 +13,18 @@ describe("voting", () => {
   const wallet = provider.wallet as anchor.Wallet;
 
   it("Initialized poll!", async () => {
-    const tx = program.methods.initalizePoll(
+    const tx =await program.methods.initalizePoll(
         new anchor.BN(1),
         new anchor.BN(0),
         new anchor.BN(1759916844),
         "What is you favorite peanut butter"
     ).accounts({signer:wallet.publicKey}).rpc()
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer,'le',8)],
+      program.programId
+    )
+    const poll = await program.account.poll.fetch(pollAddress);
 
+    console.log(JSON.stringify(poll, null, 2));
   });
 });
