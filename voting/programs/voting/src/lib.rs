94ea07e24ddc03da54f0e0664a42f90504a4105d
mikeyyy12 <shubhamkarmyal95@gmail.com>
2025-10-08 15:10:27 +0530

    Initalize poll

--- voting/programs/voting/src/lib.rs
+++ voting/programs/voting/src/lib.rs
@@ -6,11 +6,47 @@ declare_id!("GE3MxRYAUSC44XE71fHgWe5fND1m3DLFAYkn5ijUUnai");
 pub mod voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initalize_poll(
+        ctx: Context<InitializePoll>,
+        poll_id: u64,
+        poll_start: u64,
+        poll_end: u64,
+        description: String,
+    ) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.poll_start = poll_start;
+        poll.description = description;
+        poll.poll_end = poll_end;
+        poll.candidate_amount = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(poll_id:u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer=signer,
+        space=8+Poll::INIT_SPACE,
+        seeds=[poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
