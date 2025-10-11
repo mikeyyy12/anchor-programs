0f1d623dbb3a0ade30fbfbfc05cf68f875a03df6
mikeyyy12 <shubhamkarmyal95@gmail.com>
2025-10-11 20:42:33 +0530

    Crud smart contract done

--- crud/programs/crud/src/lib.rs
+++ crud/programs/crud/src/lib.rs
@@ -6,11 +6,88 @@ declare_id!("CnrrWu9XDxHnQcJmRLY22c1KWW8uRy6Ao3WtFfWwgweZ");
 pub mod crud {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_journal_entry(ctx:Context<CreateEntry>,title:String,message:String)->Result<()>{
+
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = *ctx.accounts.owner.key;
+        journal_entry.title = title;
+        journal_entry.message = message;
+
+        Ok(())
+    }
+    pub fn update_journal_entry(ctx:Context<UpdateEntry>,_title:String,message:String)->Result<()>{
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.message = message;
+
+        Ok(())
+    }
+
+    pub fn delete_journal_entry(ctx:Context<DeleteEntry,_title:String)->Result<()>{
+
         Ok(())
     }
 }
 
+#[derive(accounts)]
+#[instruction(title:String)]
+pub struct DeleteEntry<'info>
+  #[account(mut,
+    seeds=seeds=[title.as_bytes(),owner.key().as_ref()],
+    bump,
+    close=owner
+)]
+  pub journal_entry<'info,JournalEntryState>,
+
+    #[account(mut)]
+    pub owner:Signer<'info>,
+
+    pub system_program:Program<'info,System>
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title:String)]
+pub struct UpdateEntry<'info>{
+
+    #[account(mut,
+    seeds=seeds=[title.as_bytes(),owner.key().as_ref()],
+        bump,
+        realloc = 8 + JournalEntryState::INIT_SPACE,
+        realloc::payer = owner,
+        realloc::Zero = true,
+    )]
+        pub journal_entry<'info,JournalEntryState>,
+
+    #[account(mut)]
+    pub owner:Signer<'info>,
+
+    pub system_program:Program<'info,System>
+}
+
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct CreateEntry<'info>{
+    #[account(
+        init,
+        seeds=[title.as_bytes(),owner.key().as_ref()],
+        bump,
+        space = 8 +JournalEntryState::INIT_SPACE,
+        payer=owner,
+    )]
+    pub journal_entry<'info,JournalEntryState>,
+
+    #[account(mut)]
+    pub owner:Signer<'info>,
+
+    pub system_program:Program<'info,System>
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+    pub owner:Pubkey;
+    #[max_len(50)]
+    pub title:String;
+    #[max_len(1000)]
+    pub message:String
+
+}
